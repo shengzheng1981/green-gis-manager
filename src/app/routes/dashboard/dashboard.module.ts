8ac841772466227928713601d04e3d66a7ee5cab
shengzheng1981 <11639583@qq.com>
2020-12-07 11:24:38 +0800

    master

--- src/app/routes/dashboard/dashboard.module.ts
+++ src/app/routes/dashboard/dashboard.module.ts
@@ -5,14 +5,6 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 
 import {NgZorroAntdModule} from 'ng-zorro-antd';
-import {AccordionModule} from 'ngx-bootstrap/accordion';
-import {ButtonsModule} from 'ngx-bootstrap/buttons';
-import {ModalModule} from 'ngx-bootstrap/modal';
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
-import {PaginationModule} from 'ngx-bootstrap/pagination';
-import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
-import {CollapseModule} from 'ngx-bootstrap/collapse';
 
 import { FeatureComponent } from './feature/feature.component';
 import { GeojsonComponent } from './geojson/geojson.component';
@@ -74,15 +66,7 @@ const routes: Routes = [
         CommonModule,
         FormsModule,
         RouterModule.forChild(routes),
-        NgZorroAntdModule,
-        AccordionModule.forRoot(),
-        ButtonsModule.forRoot(),
-        ModalModule.forRoot(),
-        TooltipModule.forRoot(),
-        BsDropdownModule.forRoot(),
-        PaginationModule.forRoot(),
-        ProgressbarModule.forRoot(),
-        CollapseModule.forRoot()
+        NgZorroAntdModule
     ],
     declarations: [DashboardComponent, FeatureComponent, GeojsonComponent, BackendComponent, FrontendComponent, SymbolComponent, StaticComponent, MapComponent, StreamComponent]
 })
